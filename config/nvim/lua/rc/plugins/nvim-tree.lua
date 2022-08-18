9870c89ec9331d67137f57c97033333fdbf04ee2
teco-otaki <otaki.keiichi@tecotec.co.jp>
2022-08-18 12:13:52 +0900

    nvim-tree設定

--- config/nvim/lua/rc/plugins/nvim-tree.lua
+++ config/nvim/lua/rc/plugins/nvim-tree.lua
@@ -148,7 +148,7 @@ require('nvim-tree').setup({
             },
         },
         open_file = {
-            quit_on_open = true,
+            quit_on_open = false,
             resize_window = true,
             window_picker = {
                 enable = true,
