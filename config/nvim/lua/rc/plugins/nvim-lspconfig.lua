69b900b0633867b2589f4fd77a4b52eaf92a1ca1
teco-otaki <otaki.keiichi@tecotec.co.jp>
2023-11-10 10:09:39 +0900

    protoのlsp追加

--- config/nvim/lua/rc/plugins/nvim-lspconfig.lua
+++ config/nvim/lua/rc/plugins/nvim-lspconfig.lua
@@ -12,16 +12,25 @@ lspconfig.gopls.setup {
 }
 
 lspconfig.intelephense.setup {
-    autostart = true,
     on_attach = function(client, bufnr)
         -- [[ other on_attach code ]]
         require('illuminate').on_attach(client)
 
         if client.server_capabilities['documentSymbolProvider'] then
-            vim.notify('intelephense.navic.attach')
             require('nvim-navic').attach(client, bufnr)
         end
     end,
 }
+
+--lspconfig.bufls.setup {
+--    on_attach = function(client, bufnr)
+--        -- [[ other on_attach code ]]
+--        require('illuminate').on_attach(client)
+--
+--        if client.server_capabilities['documentSymbolProvider'] then
+--            require('nvim-navic').attach(client, bufnr)
+--        end
+--    end,
+--}
 --lspconfig.sumneko_lua.setup {}
 --lspconfig.tsserver.setup {}
