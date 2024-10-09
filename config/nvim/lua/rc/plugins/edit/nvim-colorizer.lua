3ff27854b876e2bf1cdcf877548c916bc16b3db7
choco-kei <choco.kei@gmail.com>
2024-10-09 13:06:30 +0900

    nvim-colorizerを追加

---
+++ config/nvim/lua/rc/plugins/edit/nvim-colorizer.lua
@@ -0,0 +1,19 @@
+return {
+    {
+        "norcalli/nvim-colorizer.lua",
+        event = {
+            "BufReadPost",
+            "BufNewFile",
+        },
+        enabled = true,
+        init = function()
+            vim.opt.termguicolors = true
+        end,
+        opts = {
+            "css",
+            "scss",
+            "javascript",
+            "html",
+        },
+    },
+}
