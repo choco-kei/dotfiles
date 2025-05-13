29d6689fa2556d0154edce461b8a48dae01671be
choco-kei <choco.kei@gmail.com>
2025-05-13 10:58:02 +0900

    init.lua 改善

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,6 +1,6 @@
 vim.loader.enable()
 
-vim.o.langmenu = os.getenv('LANG')
+vim.o.langmenu = os.getenv('LANG') or 'ja_JP.UTF-8'
 
 vim.o.encoding = 'utf-8'
 vim.o.fileencodings = 'utf-8,cp932,euc-jp,iso-20220-jp,default,latin,sjis'
@@ -19,12 +19,15 @@ require('rc/pluginlist')
 require('rc/command')
 require('rc/autocmd')
 
--- ローカル設定
-if vim.fn.filereadable(vim.fn.expand('~/.nvim.local.lua')) ~= 0 then
-    dofile(vim.fn.expand('~/.nvim.local.lua'))
+
+local function load_if_exists(file)
+  if vim.fn.filereadable(vim.fn.expand(file)) ~= 0 then
+    dofile(vim.fn.expand(file))
+  end
 end
 
+-- ローカル設定
+load_if_exists('~/.nvim.local.lua')
+
 -- プロジェクト設定
-if vim.fn.filereadable('.nvim.project.lua') ~= 0 then
-    dofile('.nvim.project.lua')
-end
+load_if_exists('.nvim.project.lua')
