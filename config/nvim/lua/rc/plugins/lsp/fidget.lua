7dc8225408bbb98bf817f9250af585a2656a7bd0
choco-kei <choco.kei@gmail.com>
2025-09-30 13:16:56 +0900

    feat: fidgetでnvimの通知を出せるように

--- config/nvim/lua/rc/plugins/lsp/fidget.lua
+++ config/nvim/lua/rc/plugins/lsp/fidget.lua
@@ -2,6 +2,18 @@ return {
     "j-hui/fidget.nvim",
     event = "VimEnter",
     config = function()
-        require("fidget").setup()
+        local fidget_notification = require("fidget.notification")
+
+        require("fidget").setup({
+            notification = {
+                override_vim_notify = true,
+                configs = {
+                    -- 通知グループごとの設定
+                    default = vim.tbl_extend("force", fidget_notification.default_config, {
+                        ttl = 8,
+                    }),
+                },
+            },
+        })
     end,
 }
