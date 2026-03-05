7aab2da2f85f557ccc240f5c5e395a62eaad67eb
choco-kei <choco.kei@gmail.com>
2026-03-05 21:02:26 +0900

    feat(toggleterm): 表示領域調整

--- config/nvim/lua/rc/plugins/terminal/toggleterm.lua
+++ config/nvim/lua/rc/plugins/terminal/toggleterm.lua
@@ -7,6 +7,10 @@ return {
             direction = "horizontal",
             -- ターミナルが開かれた時に実行する処理を追加
             on_open = function(term)
+                vim.opt_local.signcolumn = "no"
+                vim.opt_local.statuscolumn = ""
+                vim.opt_local.foldcolumn = "0"
+
                 -- ターミナルモードで <C-o> を <C-\><C-n> にマッピングする
                 -- この設定は、このターミナルバッファ内でのみ有効になります
                 vim.keymap.set('t', '<C-o>', [[<C-\><C-n>]], {
@@ -14,6 +18,8 @@ return {
                     silent = true,
                     buffer = term.bufnr,
                 })
+                vim.api.nvim_command("startinsert")
+                vim.api.nvim_command("checktime")
             end,
         })
     end,
