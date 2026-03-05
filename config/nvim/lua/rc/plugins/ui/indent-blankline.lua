b647ce39a33ef238e702bc822ea564fb4fef8f5f
choco-kei <choco.kei@gmail.com>
2026-03-05 13:01:07 +0900

    feat(ibl): iblを全体的に調整、tab用の表記追加など

--- config/nvim/lua/rc/plugins/ui/indent-blankline.lua
+++ config/nvim/lua/rc/plugins/ui/indent-blankline.lua
@@ -1,26 +1,43 @@
 return {
-    "lukas-reineke/indent-blankline.nvim",
-    event = "VimEnter",
-    dependencies = { COLOR_SCHEME },
-    config = function()
-        local highlight = {
-            "IndentBlanklineIndent1",
-            "IndentBlanklineIndent2",
-            "IndentBlanklineIndent3",
-            "IndentBlanklineIndent4",
-            "IndentBlanklineIndent5",
-            "IndentBlanklineIndent6",
-        }
-        local hooks = require("ibl.hooks")
+  "lukas-reineke/indent-blankline.nvim",
+  main = "ibl",
+  event = "VeryLazy",
+  dependencies = { COLOR_SCHEME },
+  ---@module "ibl"
+  ---@type ibl.config
+  opts = {
+    indent = {
+      char     = "│",
+      -- tab_char = "╽",
+      -- tab_char = "┼",
+      tab_char = "├",
+      highlight = "IBLIndent",
+    },
+    scope = {
+      enabled = true,
+      -- スコープの色リスト
+      highlight = {
+        "IndentBlanklineIndent1",
+        "IndentBlanklineIndent2",
+        "IndentBlanklineIndent3",
+        "IndentBlanklineIndent4",
+        "IndentBlanklineIndent5",
+        "IndentBlanklineIndent6",
+      },
+      show_start = true,
+      show_end = false,
+    },
+  },
+  config = function(_, opts)
+    local hooks = require("ibl.hooks")
 
-        vim.g.rainbow_delimiters = { highlight = highlight }
-        require("ibl").setup({
-            scope = { highlight = highlight },
-            indent = {
-                char = "│",
-            },
-        })
+    vim.g.rainbow_delimiters = { highlight = opts.scope.highlight }
 
-        hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
-    end,
+    hooks.register(
+      hooks.type.SCOPE_HIGHLIGHT,
+      hooks.builtin.scope_highlight_from_extmark
+    )
+
+    require("ibl").setup(opts)
+  end,
 }
