38f710e9588ee461588772d01dc347ef860de6c9
choco-kei <choco.kei@gmail.com>
2026-04-06 21:56:35 +0900

    feat(leap): 最終的にflashからleapに変更

--- config/nvim/lua/rc/plugins/search/leap.lua
+++ config/nvim/lua/rc/plugins/search/leap.lua
@@ -2,15 +2,48 @@ return {
   url = "https://codeberg.org/andyg/leap.nvim",
   name = "leap.nvim",
   event = "VeryLazy",
-  enabled = false,
+  enabled = true,
   config = function()
     local leap = require("leap")
 
-    -- 1. sj: 2文字検索
-    vim.keymap.set({ "n", "x", "o" }, "sj", "<Plug>(leap)", {desc = "Leap: 2-char search"})
+    leap.opts.preview = false
+    local hl = vim.api.nvim_get_hl(0, { name = "DiagnosticVirtualTextWarn", link = false })
+    vim.api.nvim_set_hl(0, "LeapLabel", vim.tbl_extend("force", hl, {
+      underline = true,
+    }))
+    leap.init_hl()
 
-    -- 2. f/F: 1文字検索
-    vim.keymap.set({"n", "x", "o"}, "f", "<Plug>(leap-forward-to)", {desc = "Leap: 1-char search forward"})
-    vim.keymap.set({"n", "x", "o"}, "F", "<Plug>(leap-backward-to)", {desc = "Leap: 1-char search backward"})
+    -- ラベルをキーの位置に表示
+    leap.opts.on_beacons = function(targets, _, _)
+      for _, t in ipairs(targets) do
+        if t.label and t.beacon then t.beacon[1] = 0 end
+      end
+    end
+
+    -- 1. sj: 2文字検索
+    vim.keymap.set({ "n", "x", "o" }, "sj", function()
+      leap.leap({
+        target_windows = { vim.api.nvim_get_current_win() },
+        opts = { safe_labels = "" },
+      })
+    end, {desc = "Leap: 2-char search"})
+
+    -- 2. f/F: 1文字検索 (READMEのenhanced f/F)
+    local function ft(key_specific_args)
+      leap.leap(vim.tbl_deep_extend("keep", key_specific_args or {}, {
+        inputlen = 1,
+        inclusive = true,
+        opts = {
+          safe_labels = "",
+        },
+      }))
+    end
+
+    vim.keymap.set({ "n", "x", "o" }, "f", function()
+      ft()
+    end, { desc = "Leap: 1-char search forward" })
+    vim.keymap.set({ "n", "x", "o" }, "F", function()
+      ft({ backward = true })
+    end, { desc = "Leap: 1-char search backward" })
   end,
 }
